a3e5e8216b6280af6dffc615d775591dea2ce872
abraham aranda <a.aranda@cohaerentis.com>
2014-05-26 16:53:25 +0200

    confirmsent user message view

--- app/views/user/confirmsent.php
+++ app/views/user/confirmsent.php
@@ -1,3 +1,16 @@
 TODO : Show sign up ok message<br>
 TODO : Inform to user that will receive a confirmation email<br>
 TODO : Ask user to follow email instruction (click at confirmation link)<br>
+
+<article class="confirm-wrapper">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-xs-12 page-title">
+            <h2><?php Yii::t('app', 'Registy Complete'?> <i class="glyphicon glyphicon-ok"></i></h2>
+        </div>
+        <div class="col-lg-12 col-md-12 col-xs-12 confirm">
+            <p><?php Yii::t('app', 'You will receive a confirmation email to validate your account.'?></p>
+            <p><?php Yii::t('app', 'Follow the instructions in it and click the link provided to end the registry.'?></p>
+        </div>
+    </div>
+  </article>
+
