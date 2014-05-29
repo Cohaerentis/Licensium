eeb8a3eca044e8abab93698ac4d74c288eb523d4
abraham aranda <a.aranda@cohaerentis.com>
2014-05-29 10:13:11 +0200

    Target page text

--- app/views/site/pages/target.php
+++ app/views/site/pages/target.php
@@ -6,7 +6,122 @@ $this->breadcrumbs=array(
     'About',
 );
 ?>
-<h1>About</h1>
-
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<article class="target-wrapper">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-xs-12 page-title">
+            <h2><?php echo Yii::t('app', 'Target');?> <i class="glyphicon "></i></h2>
+        </div>
+        <div class="col-lg-12 col-md-12 col-xs-12 target-text">
+            <p>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'One of the biggest problems when we perform a compatibility license diagnostic consultancy is access to a clear
+                         and detailed list of licenses whose code has been used in development.'
+                    );
+                ?>
+            </p>
+            <p>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Therefore we always recommend as a "good practice" to be performed by the companies keep an inventory of 
+                        such licenses attaching, if possible, the contents thereof by the time the code is linked or integrated.'
+                    );
+                ?>
+            </p>
+            <p>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'While this is a task that can be performed easily with the most popular office tools, we wanted to go one 
+                        step foward and developed a tool that make this work to programmers easier, adding value to the inventory. 
+                        Licensium has as targets the following functionalities:'
+                    );
+                ?>
+            </p>
+            <br />
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Maintain an inventory of licenses used'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Register a private developer account'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Registry and project editing'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Registry and project licenses editing'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Access to other consultants'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Attach files that contain such licenses'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Link the source code used to the license model'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Provide the posibility of checking compatibilty between licenses being used'
+                    );
+                ?>
+            </p>
+            <p>
+                <i class="glyphicon glyphicon-arrow-right"></i>
+                <?php
+                    echo Yii::t(
+                        'app',
+                        'Provide a development license choice tool through three criteria: licenses compatibilty, ceographical scope and commercial interests.'
+                    );
+                ?>
+            </p>
+        </div>
+    </div>
+</article>
