df3926cabb3763b041996ae21213539acb6374a3
aespinosa <aespinosa@teachnova.com>
2014-06-05 00:22:12 +0200

    Show ID and link to module list

--- app/views/project/view.php
+++ app/views/project/view.php
@@ -6,6 +6,12 @@
 <div class="row">
     <div class="col-md-12">
         <div class="row">
+            <div class="label col-lg-12 col-md-6"><?php echo e($model->getAttributeLabel('id')); ?></div>
+            <div class="value col-lg-12 col-md-6"><?php echo e($model->id); ?></div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="row">
             <div class="label col-lg-12 col-md-6"><?php echo e($model->getAttributeLabel('name')); ?></div>
             <div class="value col-lg-12 col-md-6"><?php echo e($model->name); ?></div>
         </div>
@@ -27,5 +33,10 @@
             <div class="label col-lg-12 col-md-6"><?php echo e($model->getAttributeLabel('createdate')); ?></div>
             <div class="value col-lg-12 col-md-6"><?php echo e($model->createDatePrint()); ?></div>
         </div>
+    </div>
+    <div class="col-md-12">
+        <a href="/module/index/projectid/<?php echo e($model->id); ?>" class="btn">
+            <?php echo Yii::t('app', 'Project modules'); ?>
+        </a>
     </div>
 </div>
