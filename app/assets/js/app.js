6e54b08d74c475e049fe407eb9418ee124cf2c83
abraham aranda <a.aranda@cohaerentis.com>
2014-06-11 15:12:37 +0200

    Script optimized and commented

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -5,9 +5,10 @@
         });
 
         $(".switcher").click(function(){
-            $(this).parent().toggleClass("off-element");
-            $(this).addClass("on-element");
-            $(this).toggleClass("switcher-off");
+            $(this).parent().toggleClass("off-element"); /* Disable the events for the parents of the "eye" (li) */
+            $(this).addClass("on-element"); /* Enable the events on the eye */
+            $(this).toggleClass("switcher-on"); /* Hides or shows the class depending on its previous state */
+            $(this).toggleClass("switcher-off"); /* Hides or shows the class depending on its previous state */
         });
 
     });
