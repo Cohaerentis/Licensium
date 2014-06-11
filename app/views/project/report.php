89c5d0a48dcf5d035721877c2b47ca58625797e0
abraham aranda <a.aranda@cohaerentis.com>
2014-06-11 15:15:34 +0200

    Print button added to report

--- app/views/project/report.php
+++ app/views/project/report.php
@@ -5,3 +5,9 @@
 TODO : Show project report info and status<br>
 TODO : Show all project modules and status<br>
 This is a public access, avoid to show personal user info
+
+
+<a href="#" class=" btn btn-success right print">
+    <i class="glyphicon glyphicon-print set-right" data-toggle="tooltip"
+    data-original-title="<?php echo Yii::t('app', 'PRINT REPORT'); ?>"></i>
+</a>
