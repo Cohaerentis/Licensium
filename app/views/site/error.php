0f402ea94cff9676c73b2d01ef25fd33b9c9a606
abraham aranda <a.aranda@cohaerentis.com>
2014-06-17 10:30:56 +0200

    Error pages added

--- app/views/site/error.php
+++ app/views/site/error.php
@@ -6,10 +6,35 @@ $this->pageTitle=Yii::app()->name . ' - Error';
 $this->breadcrumbs=array(
     'Error',
 );
+
+$msg = '';
+if($code==404) {
+    $msg = Yii::t('app', 'May be your memory is getting worse or perhaps your fingers are too fat...');
+}
+if($code==401) {
+    $msg = Yii::t('app', 'Oh, dude!! That´s not the way. But you can keep trying.');
+}
+
 ?>
 
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
+<div class="center-error">
+    <div class="row">
+    <div class="col-lg-12">
+        <div class="back">
+            <div class="error-title">
+                <h2>Error <?php echo $code; ?></h2>
+            </div>
+            <div class="error">
+                <strong><?php echo CHtml::encode($message); ?></strong>
+                <p><?php echo $msg; ?></p>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-12">
+        <div id='monster'>
+            <img src="/img/monster.png"/>
+        </div>
+    </div>
+    </div>
 </div>
+
