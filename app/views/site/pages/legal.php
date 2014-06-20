1fdabe2d1588953a6bd4c81c8a1628ad67cfbb4f
abraham aranda <a.aranda@cohaerentis.com>
2014-06-20 10:17:41 +0200

    Legal page bugs fixed.

--- app/views/site/pages/legal.php
+++ app/views/site/pages/legal.php
@@ -17,50 +17,49 @@ $this->breadcrumbs=array(
             <div class="legal-text">
 
                <div> 
-                    <h3><?php echo Yii::t('app', 'Aviso lega'?>l</h3>
+                    <h3><?php echo Yii::t('app', 'Aviso legal')?></h3>
                </div>
 
-                <p><?php echo Yii::t('app', 'El titular de este sitio web es Cohaerentis S.L Con domicilio a efectos de notificaciones en la Plaza del Conde del Valle de Súchil. CP. 28015. Madrid. CIF: B86107356.'?></p>
-                <p><?php echo Yii::t('app', 'Teléfono +34 91375 03 91'?>.<?php echo Yii::t('app', ' Horario de atención al público. De 10 a 14.00 y de 15.30 a 18.99'?></p>
+                <p><?php echo Yii::t('app', 'El titular de este sitio web es Cohaerentis S.L Con domicilio a efectos de notificaciones en la Plaza del Conde del Valle de Súchil. CP. 28015. Madrid. CIF: B86107356.')?></p>
+                <p><?php echo Yii::t('app', 'Teléfono +34 91375 03 91')?>.<?php echo Yii::t('app', ' Horario de atención al público. De 10 a 14.00 y de 15.30 a 18.99')?></p>
 
                 <div>
-                    <h3><?php echo Yii::t('app', 'Condiciones de uso y navegación'?></h3>
+                    <h3><?php echo Yii::t('app', 'Condiciones de uso y navegación')?></h3>
                 </div>
 
-                <p><?php echo Yii::t('app', 'El acceso a los contenidos proporcionados a través del presente sitio web tiene carácter gratuito. La utilización de las funcionalidades del sitio requieren registro previo, lo cual requerirá la selección
-                por parte del usuario de un identificador y contraseña'?>.</p>
-                <p><?php echo Yii::t('app', 'La contraseña debe ser personal e intransferible para cada usuario registrado. El usuario se compromete a mantener de manera adecuada la custodia y secreto de su contraseña de acceso, sin comunicarla a terceros. Será responsabilidad exclusiva del usuario la utilización ilícita del portal web por cualquier tercero no autorizado que utilice la contraseña a causa de la comunicación o pérdida por parte del usuario'?>.</p>
-                <p><?php echo Yii::t('app', 'En caso de pérdida de la contraseña, el usuario debe comunicar dicha cuestión a los webmaster para proceder a la inmediata cancelación de la misma'?>.</p>
+                <p><?php echo Yii::t('app', 'El acceso a los contenidos proporcionados a través del presente sitio web tiene carácter gratuito. La utilización de las funcionalidades del sitio requieren registro previo, lo cual requerirá la selección por parte del usuario de un identificador y contraseña')?>.</p>
+                <p><?php echo Yii::t('app', 'La contraseña debe ser personal e intransferible para cada usuario registrado. El usuario se compromete a mantener de manera adecuada la custodia y secreto de su contraseña de acceso, sin comunicarla a terceros. Será responsabilidad exclusiva del usuario la utilización ilícita del portal web por cualquier tercero no autorizado que utilice la contraseña a causa de la comunicación o pérdida por parte del usuario')?>.</p>
+                <p><?php echo Yii::t('app', 'En caso de pérdida de la contraseña, el usuario debe comunicar dicha cuestión a los webmaster para proceder a la inmediata cancelación de la misma')?>.</p>
                 <div>
-                    <h3><?php echo Yii::t('app', 'Propiedad intelectual e industrial'?>:</h3>
+                    <h3><?php echo Yii::t('app', 'Propiedad intelectual e industrial')?>:</h3>
                 </div>
-                <p><?php echo Yii::t('app', 'El presente sitio se rige por la legislación española en materia de propiedad intelectual e industrial y por la legislación internacional aplicable'?>.</p>
-                <p><?php echo Yii::t('app', 'Los derechos de propiedad intelectual sobre la aplicación web caen bajo licencia AGL v3 y los fuentes pueden descargarse del siguiente'?> <a target="_blank" href="https://github.com/Teachnova/Licensium">
-                <?php echo Yii::t('app', 'repositorio'?></a>.</p>
+                <p><?php echo Yii::t('app', 'El presente sitio se rige por la legislación española en materia de propiedad intelectual e industrial y por la legislación internacional aplicable')?>.</p>
+                <p><?php echo Yii::t('app', 'Los derechos de propiedad intelectual sobre la aplicación web caen bajo licencia AGL v3 y los fuentes pueden descargarse del siguiente')?> <a target="_blank" href="https://github.com/Teachnova/Licensium">
+                <?php echo Yii::t('app', 'repositorio')?></a>.</p>
 
-                <p><?php echo Yii::t('app', 'Los signos distintivos utilizados en el portal son titularidad de Cohaerentis S.L. o posee la correspondiente licencia o autorización sobre dichos signos para usarlos en el portal. No está permitida su utilización sin autorización previa y por escrito de Cohaerentis S.L.'?></p>
+                <p><?php echo Yii::t('app', 'Los signos distintivos utilizados en el portal son titularidad de Cohaerentis S.L. o posee la correspondiente licencia o autorización sobre dichos signos para usarlos en el portal. No está permitida su utilización sin autorización previa y por escrito de Cohaerentis S.L.')?></p>
                 <div>
-                    <h3><?php echo Yii::t('app', 'Limitación de responsabilidad'?>:</h3>
+                    <h3><?php echo Yii::t('app', 'Limitación de responsabilidad')?>:</h3>
                 </div>
-                <p><?php echo Yii::t('app', 'Los contenidos preparados por Cohaerentis S.L. en este sitio web tienen fines informativos y divulgativos. Dichos contenidos no constituyen un asesoramiento legal o consultoría jurídica de ningún tipo y pueden contener errores'?>.</p>
-                <p><?php echo Yii::t('app', 'Por tanto, la empresa no se responsabiliza de las actuaciones que se lleven a cabo en función de la información obtenida en este portal'?>.</p>
-                <p><?php echo Yii::t('app', 'Si requiere asesoramiento jurídico póngase en contacto con nosotros a tal efecto'?>.</p>
+                <p><?php echo Yii::t('app', 'Los contenidos preparados por Cohaerentis S.L. en este sitio web tienen fines informativos y divulgativos. Dichos contenidos no constituyen un asesoramiento legal o consultoría jurídica de ningún tipo y pueden contener errores')?>.</p>
+                <p><?php echo Yii::t('app', 'Por tanto, la empresa no se responsabiliza de las actuaciones que se lleven a cabo en función de la información obtenida en este portal')?>.</p>
+                <p><?php echo Yii::t('app', 'Si requiere asesoramiento jurídico póngase en contacto con nosotros a tal efecto')?>.</p>
                 <div>
-                <h3><?php echo Yii::t('app', 'Protección de datos de carácter personal'?>:</h3>
+                <h3><?php echo Yii::t('app', 'Protección de datos de carácter personal')?>:</h3>
                 </div>
-                <p><?php echo Yii::t('app', 'El tratamiento de datos de carácter personal se rige por la política de privacidad definida por Cohaerentis S.L. que puede consultarse en este'?> <a href="/site/page/view/policy"><?php echo Yii::t('app', 'enlace'?></a>.</p>
+                <p><?php echo Yii::t('app', 'El tratamiento de datos de carácter personal se rige por la política de privacidad definida por Cohaerentis S.L. que puede consultarse en este')?> <a href="/site/page/view/policy"><?php echo Yii::t('app', 'enlace')?></a>.</p>
                 <div>
-                    <h4><strong><?php echo Yii::t('app', 'Actividad profesional'?>:</strong></h4>
+                    <h4><strong><?php echo Yii::t('app', 'Actividad profesional')?>:</strong></h4>
                 </div>
-                <p><?php echo Yii::t('app', 'Los abogados de Cohaerentis S.L. pertenecen al Ilustre Colegio de Abogados de Madrid'?>.</p>
-                <p><?php echo Yii::t('app', 'Normas y códigos de conducta aplicables: normativa del ICAM'?>.</p>
-                <p><?php echo Yii::t('app', 'Son aplicables al ejercicio de la abogacía el Estatuto General de la Abogacía Española y el Código Deontológico'?>.</p> 
-                <p><?php echo Yii::t('app', 'Dichas normas se encuentran en el sitio web del Consejo General de la Abogacía'?>.</p>
+                <p><?php echo Yii::t('app', 'Los abogados de Cohaerentis S.L. pertenecen al Ilustre Colegio de Abogados de Madrid')?>.</p>
+                <p><?php echo Yii::t('app', 'Normas y códigos de conducta aplicables: normativa del ICAM')?>.</p>
+                <p><?php echo Yii::t('app', 'Son aplicables al ejercicio de la abogacía el Estatuto General de la Abogacía Española y el Código Deontológico')?>.</p> 
+                <p><?php echo Yii::t('app', 'Dichas normas se encuentran en el sitio web del Consejo General de la Abogacía')?>.</p>
                 <div>
-                    <h4><strong><?php echo Yii::t('app', 'Legislación aplicable'?>:</strong></h4>
+                    <h4><strong><?php echo Yii::t('app', 'Legislación aplicable')?>:</strong></h4>
                 </div>
-                <p><?php echo Yii::t('app', 'Las relaciones entre el usuario y Cohaerentis S.L. se regirán por lo dispuesto en la normativa vigente sobre la legislación aplicable'?>.</p>
-                <p><?php echo Yii::t('app', 'Para los supuestos en que la normativa prevea la posibilidad de someterse a un fuero concreto, Cohaerentis Consultores y el usuario someterán sus controversias, con renuncia expresa a cualquier otro foro que pudiera corresponderles, a los juzgados y tribunales de Madrid capital'?>.</p>
+                <p><?php echo Yii::t('app', 'Las relaciones entre el usuario y Cohaerentis S.L. se regirán por lo dispuesto en la normativa vigente sobre la legislación aplicable')?>.</p>
+                <p><?php echo Yii::t('app', 'Para los supuestos en que la normativa prevea la posibilidad de someterse a un fuero concreto, Cohaerentis Consultores y el usuario someterán sus controversias, con renuncia expresa a cualquier otro foro que pudiera corresponderles, a los juzgados y tribunales de Madrid capital')?>.</p>
             </div>
         </div>
     </div>
