63aa34899d3863afb49bcf1dd16532c38050ba2b
abraham aranda <a.aranda@cohaerentis.com>
2014-06-30 09:31:53 +0200

    Unit Tests

--- app/models/User.php
+++ app/models/User.php
@@ -193,11 +193,22 @@ class User extends CActiveRecord {
         }
         return false;
     }
-
+/* - AAA -
     public function emailChange($newemail) {
         $emailvalidator = new CEmailValidator();
         if ( ($this->email != $newemail) && ($emailvalidator->validateValue($newemail)) ) {
             $this->emailold = $newemail;
+            return $this->secretSet();
+        }
+
+        return false;
+    }
+*/
+    public function emailChange($newemail) {
+        $emailvalidator = new CEmailValidator();
+        if ( ($this->email != $newemail) && ($emailvalidator->validateValue($newemail)) ) {
+            $this->emailold = $this->email;
+            $this->email = $newemail;
             return $this->secretSet();
         }
 
